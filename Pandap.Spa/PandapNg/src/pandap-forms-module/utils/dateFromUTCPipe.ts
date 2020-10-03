9d424c5eaf4f6e1f54583b9cc07e292b727e0fea
gokmen.yilmaz <gokmen1977@gmail.com>
2020-10-03 19:24:24 +0300

    uu

--- Pandap.Spa/PandapNg/src/pandap-forms-module/utils/dateFromUTCPipe.ts
+++ Pandap.Spa/PandapNg/src/pandap-forms-module/utils/dateFromUTCPipe.ts
@@ -3,8 +3,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: "DateFromUTCPipe" })
 export class DateFromUTCPipe implements PipeTransform {
-    transform(value: string) {
-            return value.split("T")[0];
-    }
+  transform(value: string) {
+    if (!value)
+      return value;
+
+    return value.split("T")[0];
+  }
 
 }
