41f33af557dbd27b33e8f43b42acba90ae60c6ba
gokmen.yilmaz <gokmen1977@gmail.com>
2020-11-15 01:04:19 +0300

    uu

--- Pandap.Spa/PandapNg/src/environments/environment.prod.ts
+++ Pandap.Spa/PandapNg/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://95.9.196.88:8080/api',
-  apiUrl1: 'http://192.168.1.203:8080/api',
+  apiUrl1: 'http://95.9.196.88:8080/api',
+  apiUrl: 'http://192.168.1.203:8080/api',
 };
