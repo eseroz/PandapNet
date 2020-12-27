2c796eaf3c04eac1ef1e0c99a17a15cf305ac4ec
gokmen.yilmaz <gokmen1977@gmail.com>
2020-12-28 02:18:00 +0300

    uu

--- Pandap.Spa/PandapNg/src/hdepo/depo-sayim/depo-sayim.component.ts
+++ Pandap.Spa/PandapNg/src/hdepo/depo-sayim/depo-sayim.component.ts
@@ -6,64 +6,66 @@ import { strict } from 'assert';
 @Component({
   selector: 'app-depo-sayim',
   templateUrl: './depo-sayim.component.html',
-  styleUrls: ['./depo-sayim.component.css']
+  styleUrls: ['./depo-sayim.component.css'],
 })
 export class DepoSayimComponent implements OnInit {
+  StokListe = [];
+  errorMessage = '';
 
-  StokListe=[];
-  errorMessage="";
+  constructor(public dataService: HdepoService) {}
 
-  constructor(public dataService:HdepoService) {
-  }
+  barkodOku = () => {
 
-
-  barkodOku=()=>
-  {
-
-    if (navigator.mediaDevices && typeof navigator.mediaDevices.getUserMedia === 'function') {
-      alert("yetki");
+    if (navigator.getUserMedia) {
+      navigator.getUserMedia(
+        { video: true, audio: false },
+        function (stream) {
+          alert('yetki');
+        },
+        function (error) {
+          alert('yetki hata');
+        }
+      );
     }
 
-    Quagga.init({
-      inputStream: {
-        constraints: {
-          facingMode: 'environment' // restrict camera type
+    Quagga.init(
+      {
+        inputStream: {
+          constraints: {
+            facingMode: 'environment', // restrict camera type
+          },
+          area: {
+            // defines rectangle of the detection
+            top: '40%', // top offset
+            right: '0%', // right offset
+            left: '0%', // left offset
+            bottom: '40%', // bottom offset
+          },
         },
-        area: { // defines rectangle of the detection
-          top: '40%',    // top offset
-          right: '0%',  // right offset
-          left: '0%',   // left offset
-          bottom: '40%'  // bottom offset
+        decoder: {
+          readers: ['ean_reader'], // restrict code types
         },
       },
-      decoder: {
-        readers: ['ean_reader'] // restrict code types
-      },
-    },
-    (err) => {
-      if (err) {
-        this.errorMessage = `QuaggaJS could not be initialized, err: ${err}`;
-      } else {
-        Quagga.start();
-        Quagga.onDetected((res) => {
-          this.onBarcodeScanned(res.codeResult.code);
-        });
+      (err) => {
+        if (err) {
+          this.errorMessage = `QuaggaJS could not be initialized, err: ${err}`;
+        } else {
+          Quagga.start();
+          Quagga.onDetected((res) => {
+            this.onBarcodeScanned(res.codeResult.code);
+          });
+        }
       }
-    });
-  }
+    );
+  };
 
   async ngOnInit() {
-    this.StokListe=await this.dataService.depoSayimListeGetir();
+    this.StokListe = await this.dataService.depoSayimListeGetir();
   }
 
-  ngAfterViewInit(): void {
-   
-    
-    
-  }
+  ngAfterViewInit(): void {}
 
   onBarcodeScanned(code: string) {
     console.log(code);
   }
-
 }
