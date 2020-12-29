4dcbcf13b1070e0e8c33dc920fe6723715aef672
gokmen.yilmaz <gokmen1977@gmail.com>
2020-12-29 11:12:31 +0300

    uu

--- Pandap.Spa/PandapNg/src/environments/environment.prod.ts
+++ Pandap.Spa/PandapNg/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  apiUrlx: 'http://192.168.1.203:8080/api',
-  apiUrl: 'http://92.44.86.224:63075/api',
+  apiUrl: 'http://192.168.1.205:8080/api',
 };
--- Pandap.Spa/PandapNg/src/environments/environment.ts
+++ Pandap.Spa/PandapNg/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  apiUrlx: 'http://localhost:63075/api',
-  apiUrl: 'http://92.44.86.224:63075/api',
+  apiUrl: 'http://localhost:63075/api',
 };
